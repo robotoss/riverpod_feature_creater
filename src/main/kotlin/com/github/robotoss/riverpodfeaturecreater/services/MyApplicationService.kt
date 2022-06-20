73cb476edbb2fbf5cc3dbb335955543b5bb644d3
GitHub Action <action@github.com>
2022-06-20 18:50:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.robotoss.riverpodfeaturecreater.services
+
+import com.github.robotoss.riverpodfeaturecreater.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/services/MyProjectService.kt
+++ src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.robotoss.riverpodfeaturecreater.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.robotoss.riverpodfeaturecreater.MyBundle
 
 class MyProjectService(project: Project) {
 
