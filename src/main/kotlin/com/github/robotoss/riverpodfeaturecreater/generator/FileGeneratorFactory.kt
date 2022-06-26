d43b45fc89ca8d36e3d21e1493cdda52e595560f
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-06-26 23:06:01 +0300

    feat(Generator): Add Interactor Generator

--- src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/FileGeneratorFactory.kt
+++ src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/FileGeneratorFactory.kt
@@ -1,10 +1,12 @@
 package com.github.robotoss.riverpodfeaturecreater.generator
 
+import com.github.robotoss.riverpodfeaturecreater.generator.components.InteractorGenerator
 import com.github.robotoss.riverpodfeaturecreater.generator.components.RepositoryGenerator
 
 object FileGeneratorFactory {
     fun getFileDataGenerators(name: String): List<FileDataGenerator> {
         val repository = RepositoryGenerator(name)
-        return listOf(repository)
+        val interactor = InteractorGenerator(name)
+        return listOf(repository, interactor)
     }
 }
--- src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/components/RepositoryGenerator.kt
+++ src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/components/RepositoryGenerator.kt
@@ -6,6 +6,6 @@ import com.intellij.psi.PsiDirectory
 class RepositoryGenerator(name: String) : FileDataGenerator(name, templateName = "repository") {
     override fun fileName() = "${snakeCase()}_repository.${fileExtension()}"
 
-    override fun fileDirectory(dir: PsiDirectory) = dir.createSubdirectory("repository")
+    override fun fileDirectory(dir: PsiDirectory) = dir.createSubdirectory("data")
 
 }
