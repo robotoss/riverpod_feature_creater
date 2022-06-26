700218c4c131f5fd7ecdfe041d01c73156f92f4b
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-06-26 23:18:13 +0300

    feat(Generator): Add State Manager Generator

--- src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/FileGeneratorFactory.kt
+++ src/main/kotlin/com/github/robotoss/riverpodfeaturecreater/generator/FileGeneratorFactory.kt
@@ -2,6 +2,7 @@ package com.github.robotoss.riverpodfeaturecreater.generator
 
 import com.github.robotoss.riverpodfeaturecreater.generator.components.InteractorGenerator
 import com.github.robotoss.riverpodfeaturecreater.generator.components.RepositoryGenerator
+import com.github.robotoss.riverpodfeaturecreater.generator.components.StateManagerGenerator
 import com.github.robotoss.riverpodfeaturecreater.generator.components.StatesGenerator
 
 object FileGeneratorFactory {
@@ -9,6 +10,7 @@ object FileGeneratorFactory {
         val repository = RepositoryGenerator(name)
         val interactor = InteractorGenerator(name)
         val states = StatesGenerator(name)
-        return listOf(repository, interactor, states)
+        val stateManager = StateManagerGenerator(name)
+        return listOf(repository, interactor, states, stateManager)
     }
 }
